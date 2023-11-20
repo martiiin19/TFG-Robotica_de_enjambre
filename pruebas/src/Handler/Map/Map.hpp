38699d0da90bbb81b7cc5595992493f5487dfb90
martiiin19 <mir9@gcloud.ua.es>
2023-11-20 17:59:49 +0100

    Proyecto con tinyXML

---
+++ pruebas/src/Handler/Map/Map.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include "tinyXML2/tinyxml2.h"
+
+
+struct Map
+{
+    Map(const char *path){
+        if (map.LoadFile(path) != tinyxml2::XML_SUCCESS) {
+            std::cerr << "Error al cargar el archivo XML." << std::endl;
+        }
+    }
+
+    ~Map(){
+        
+    }
+
+    private:
+        tinyxml2::XMLDocument map;
+       
+};
